113cfec3efd4e73608757d4eb4ac9be738a6e9f0
David Besse <dahodprod@gmail.com>
2024-03-12 13:35:01 +0100

    'new solution, added api directory and using Vercel serverless function'

--- api/routes/mainRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const mainRouter = (0, express_1.Router)();
-mainRouter.get("/", (req, res) => {
-    res.send("Hello welcome to Pokedex API ! This is the main route, check /api/docs for more information");
-});
-exports.default = mainRouter;
