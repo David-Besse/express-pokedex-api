14b4761b0dadda18ed3173a8fabbd528edfb76b8
David Besse <dahodprod@gmail.com>
2024-04-22 16:45:17 +0200

    'process.env as string'

--- src/index.ts
+++ src/index.ts
@@ -30,8 +30,8 @@ const port =
 const corsOptions = {
   origin:
     process.env.NODE_ENV === "development"
-      ? process.env.CLIENT_URL
-      : process.env.CLIENT_URL_P,
+      ? process.env.CLIENT_URL as string
+      : process.env.CLIENT_URL_P as string,
   credentials: true,
 };
 app.use(cors(corsOptions));
